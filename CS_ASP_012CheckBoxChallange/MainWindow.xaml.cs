61da8b7214d69d5fc19bc85ba64c7f9373d83785
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2020-09-25 10:34:21 -0500

    final commit

--- CS_ASP_012CheckBoxChallange/MainWindow.xaml.cs
+++ CS_ASP_012CheckBoxChallange/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,22 +32,42 @@ namespace CS_ASP_012CheckBoxChallange
             if (pencilRadioButton.IsChecked == true)
             {
                 choiceLabel.Content = "Pencil, very classic choice";
+                pencilImage.Opacity = 100;
+                penImage.Opacity = 0;
+                phoneImage.Opacity = 0;
+                padImage.Opacity = 0;
             }
             else if (penRadioButton.IsChecked == true)
             {
                 choiceLabel.Content = "Pen, a bit more modern";
+                pencilImage.Opacity = 0;
+                penImage.Opacity = 100;
+                phoneImage.Opacity = 0;
+                padImage.Opacity = 0;
             }
             else if (phoneRadioButton.IsChecked == true)
             {
                 choiceLabel.Content = "Phone, aren't you special";
+                pencilImage.Opacity = 0;
+                penImage.Opacity = 0;
+                phoneImage.Opacity = 100;
+                padImage.Opacity = 0;
             }
             else if (padRadioButton.IsChecked == true)
             {
                 choiceLabel.Content = "Pad, you're really special";
+                pencilImage.Opacity = 0;
+                penImage.Opacity = 0;
+                phoneImage.Opacity = 0;
+                padImage.Opacity = 100;
             }
             else
             {
                 choiceLabel.Content = "Choose one silly!";
+                pencilImage.Opacity = 0;
+                penImage.Opacity = 0;
+                phoneImage.Opacity = 0;
+                padImage.Opacity = 0;
             }
         }
     }
